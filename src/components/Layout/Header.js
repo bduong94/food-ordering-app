58697fb8e0023a47cbc1c2685fb2ae599d5d1126
Brian Duong <brianminhduong@gmail.com>
2022-06-27 20:13:36 -0400

    Create Header Component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,9 +1,20 @@
 import React from "react";
 
 import classes from "./Header.module.css";
+import mealsImage from "../../images/meals.jpg";
 
 const Header = (props) => {
-  return <></>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table of delicious food" />
+      </div>
+    </>
+  );
 };
 
 export default Header;
