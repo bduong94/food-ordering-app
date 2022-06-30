a55c4e0ad203ba452295e9b84f173f51cadd8fad
Brian Duong <brianminhduong@gmail.com>
2022-06-29 22:29:31 -0400

    Add mark-up for Cart component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,7 +3,23 @@ import React from "react";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
-  return <div></div>;
+  const cartItems = [{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map(
+    (item) => <li>{item.name}</li>
+  );
+
+  return (
+    <div>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes.action}>
+        <button className={classes["cartItems"]}></button>
+        <button className={classes.button}></button>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
