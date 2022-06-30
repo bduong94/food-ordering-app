733d0069998f6091302a9c822d8f118438608c37
Brian Duong <brianminhduong@gmail.com>
2022-06-29 23:08:01 -0400

    Create Modal Component

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -5,7 +5,7 @@ import classes from "./HeaderCartButton.module.css";
 const HeaderCartButton = (props) => {
   return (
     <button className={classes.button}>
-      <span classname={classes.icon}>
+      <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
