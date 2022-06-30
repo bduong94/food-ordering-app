e31e4fabd06b66d00d6b5e82b3079d0c2f26cf37
Brian Duong <brianminhduong@gmail.com>
2022-06-29 21:32:32 -0400

    Change markup for MealItem component

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -4,9 +4,12 @@ import classes from "./MealItem.module.css";
 const MealItem = ({ name, description, price }) => {
   return (
     <li className={classes.meal}>
-      <h3>{name}</h3>
-      <p className={classes.description}>{description}</p>
-      <p className={classes.price}>{price}</p>
+      <div>
+        <h3>{name}</h3>
+        <div className={classes.description}>{description}</div>
+        <div className={classes.price}>{`$${price.toFixed(2)}`}</div>
+      </div>
+      <div></div>
     </li>
   );
 };
