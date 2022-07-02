0a1f652f081e9c01ec7d693893c93e654c096ae4
Brian Duong <brianminhduong@gmail.com>
2022-07-01 21:46:39 -0400

    Implement CartContext with HeaderCartButton component

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = ({ onClick }) => {
+  const cartCtx = useContext(CartContext);
+
   return (
     <button onClick={onClick} className={classes.button}>
       <span className={classes.icon}>
