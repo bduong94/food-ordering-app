6eeb9bb5f11f5b8db75cf90ca60139a7ff0a0e29
Brian Duong <brianminhduong@gmail.com>
2022-07-03 11:54:46 -0400

    Add overflow CSS to Cart component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -10,10 +10,20 @@ const Cart = ({ onCloseCart }) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
+  const cartItemRemoveHandler = (id) => {};
+  const cartItemAddHandler = (id) => {};
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
-        <CartItem key={item.id} name={item.name} price={item.price} />
+        <CartItem
+          key={item.id}
+          name={item.name}
+          price={item.price}
+          amount={item.amount}
+          onAdd={cartItemAddHandler.bind(null, item.id)}
+          onRemove={cartItemRemoveHandler(null, item)}
+        />
       ))}
     </ul>
   );
